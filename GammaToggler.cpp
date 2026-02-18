cbb0af65f890930440117b50b62278bc8c04f519
sara <pk.peterda@gmail.com>
2026-02-18 03:57:54 +0100

    a

--- GammaToggler.cpp
+++ GammaToggler.cpp
@@ -1,6 +1,6 @@
 #ifndef UNICODE
 #define UNICODE
-#endif 
+#endif
 
 #include <windows.h>
 #include <wcsplugin.h>
@@ -20,9 +20,10 @@ list<DISPLAY_DEVICEW> monitors;
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-wstring ExePath() {
-    TCHAR buffer[MAX_PATH] = { 0 };
-    GetModuleFileName( NULL, buffer, MAX_PATH );
+wstring ExePath()
+{
+    TCHAR buffer[MAX_PATH] = {0};
+    GetModuleFileName(NULL, buffer, MAX_PATH);
     wstring::size_type pos = wstring(buffer).find_last_of(L"\\/");
     return wstring(buffer).substr(0, pos);
 }
@@ -42,31 +43,31 @@ void ApplyProfile(LPCWSTR monitorDeviceID, PCWSTR profilePath)
 
     if (!WcsSetUsePerUserProfiles(monitorDeviceID, CLASS_MONITOR, TRUE))
     {
-        wcout << L"WcsSetUsePerUserProfiles error" 
-                   << GetLastError() << L"\n";
+        wcout << L"WcsSetUsePerUserProfiles error"
+              << GetLastError() << L"\n";
         return;
     }
 
     if (!WcsAssociateColorProfileWithDevice(
-        WCS_PROFILE_MANAGEMENT_SCOPE_CURRENT_USER,
-        profilePath,
-        monitorDeviceID))
+            WCS_PROFILE_MANAGEMENT_SCOPE_CURRENT_USER,
+            profilePath,
+            monitorDeviceID))
     {
         wcout << L"WcsAssociateColorProfileWithDevice error"
-                   << GetLastError() << L"\n";
+              << GetLastError() << L"\n";
         return;
     }
 
     if (!WcsSetDefaultColorProfile(
-        WCS_PROFILE_MANAGEMENT_SCOPE_CURRENT_USER,
-        monitorDeviceID,
-        CPT_ICC,
-        CPST_NONE,
-        0,
-        profilePath))
+            WCS_PROFILE_MANAGEMENT_SCOPE_CURRENT_USER,
+            monitorDeviceID,
+            CPT_ICC,
+            CPST_NONE,
+            0,
+            profilePath))
     {
         wcout << L"WcsSetDefaultColorProfile error"
-                   << GetLastError() << L"\n";
+              << GetLastError() << L"\n";
         return;
     }
 }
@@ -74,12 +75,12 @@ void ApplyProfile(LPCWSTR monitorDeviceID, PCWSTR profilePath)
 void RemoveProfile(LPCWSTR monitorDeviceID, LPCWSTR profilePath)
 {
     if (!WcsDisassociateColorProfileFromDevice(
-        WCS_PROFILE_MANAGEMENT_SCOPE_CURRENT_USER,
-        profilePath,
-        monitorDeviceID))
+            WCS_PROFILE_MANAGEMENT_SCOPE_CURRENT_USER,
+            profilePath,
+            monitorDeviceID))
     {
         wcout << L"WcsAssociateColorProfileWithDevice error"
-                   << GetLastError() << L"\n";
+              << GetLastError() << L"\n";
     }
 }
 
@@ -96,7 +97,6 @@ list<DISPLAY_DEVICEW> getMonitors()
 
         if (!EnumDisplayDevicesW(NULL, adapterIndex, &adapter, 0))
             break;
-
 
         DWORD monitorIndex = 0;
         DISPLAY_DEVICEW monitor;
@@ -128,19 +128,19 @@ wstring getProfilePath(wstring profileName)
 {
     WCHAR colorDir[MAX_PATH];
     DWORD size = MAX_PATH;
-    GetColorDirectoryW(NULL, colorDir, &size); 
+    GetColorDirectoryW(NULL, colorDir, &size);
     return wstring(colorDir) + L"\\" + profileName;
 }
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
     // Register the window class.
-    const wchar_t CLASS_NAME[]  = L"Gamma Toggler Window";
-    
-    WNDCLASS wc = { };
+    const wchar_t CLASS_NAME[] = L"Gamma Toggler Window";
 
-    wc.lpfnWndProc   = WindowProc;
-    wc.hInstance     = hInstance;
+    WNDCLASS wc = {};
+
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
     wc.lpszClassName = CLASS_NAME;
 
     RegisterClass(&wc);
@@ -148,19 +148,19 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
     // Create the window.
 
     HWND hwnd = CreateWindowEx(
-        0,                              // Optional window styles.
-        CLASS_NAME,                     // Window class
-        L"Gamma Toggler",                // Window text
-        WS_OVERLAPPEDWINDOW,            // Window style
+        0,                   // Optional window styles.
+        CLASS_NAME,          // Window class
+        L"Gamma Toggler",    // Window text
+        WS_OVERLAPPEDWINDOW, // Window style
 
         // Size and position
         CW_USEDEFAULT, CW_USEDEFAULT, 400, 250,
 
-        NULL,       // Parent window    
-        NULL,       // Menu
-        hInstance,  // Instance handle
-        NULL        // Additional application data
-        );
+        NULL,      // Parent window
+        NULL,      // Menu
+        hInstance, // Instance handle
+        NULL       // Additional application data
+    );
 
     if (hwnd == NULL)
     {
@@ -174,11 +174,9 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 
     RegisterHotKey(hwnd, 1, MOD_ALT | MOD_NOREPEAT, 0x47);
 
-
-    
     // Run the message loop.
 
-    MSG msg = { };
+    MSG msg = {};
     while (GetMessage(&msg, NULL, 0, 0) > 0)
     {
         TranslateMessage(&msg);
@@ -193,34 +191,37 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
     switch (uMsg)
     {
     case WM_HOTKEY:
+    {
+        if (gamma)
         {
-            if (gamma) {
-                for (DISPLAY_DEVICEW monitor : monitors) {
-                    RemoveProfile(monitor.DeviceID, profilePath.c_str());
-                }
-            } else {
-                for (DISPLAY_DEVICEW monitor : monitors) {
-                    ApplyProfile(monitor.DeviceID, profilePath.c_str());
-                }
+            for (DISPLAY_DEVICEW monitor : monitors)
+            {
+                RemoveProfile(monitor.DeviceID, profilePath.c_str());
             }
-            gamma = !gamma;
         }
+        else
+        {
+            for (DISPLAY_DEVICEW monitor : monitors)
+            {
+                ApplyProfile(monitor.DeviceID, profilePath.c_str());
+            }
+        }
+        gamma = !gamma;
+    }
         return 0;
 
     case WM_DESTROY:
         PostQuitMessage(0);
         return 0;
-        
+
     case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hwnd, &ps);
-            FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW+1));
-            EndPaint(hwnd, &ps);
-        }
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+        EndPaint(hwnd, &ps);
+    }
         return 0;
     }
     return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
-
-
